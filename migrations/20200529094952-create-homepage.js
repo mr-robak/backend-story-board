09a9a78e320175b3993623ea4cb9bf70fc1b1d7e
Marcin Robak <mr.robak@gmail.com>
2020-05-30 15:23:34 +0200

    update to migrations

--- migrations/20200529094952-create-homepage.js
+++ migrations/20200529094952-create-homepage.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       title: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       description: {
         type: Sequelize.TEXT,
--- migrations/20200529105122-homepage-user-relations.js
+++ migrations/20200529105122-homepage-user-relations.js
@@ -19,6 +19,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn("hemepages", "userId");
+    return queryInterface.removeColumn("homepages", "userId");
   },
 };
